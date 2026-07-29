7240690bd2c1a76363138d1967fecdfb7ff9d957
Vinh <vinh270795@gmail.com>
2026-07-29 18:20:09 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 
 android {
     namespace = "com.base.app"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.base.app"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
